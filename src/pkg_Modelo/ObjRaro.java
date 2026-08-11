37cda2c6ac0c087c302f6d3cb5fb29766f234b34
brand <brand@10.16.16.25>
2026-08-11 10:14:40 -0600

    Commit brand

---
+++ src/pkg_Modelo/ObjRaro.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg_Modelo;
+
+/**
+ *
+ * @author brand
+ */
+public class ObjRaro extends Objeto {
+    String rareza;
+    int probabilidad;
+    public ObjRaro(){
+    super();
+    }
+
+    @Override
+    public String rareza() {
+        return "raro";
+    }
+        
+}
--- src/pkg_Modelo/Objeto.java
+++ src/pkg_Modelo/Objeto.java
@@ -8,6 +8,43 @@ package pkg_Modelo;
  *
  * @author limao
  */
-public class Objeto {
+public abstract class Objeto {
+    private int idObjeto;
+    private  String nombre;
+    private String descripcion;
+
+    public Objeto() {}
+
+    public Objeto(int idObjeto, String nombre, String descripcion) {
+        this.idObjeto = idObjeto;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+    
+    public abstract String rareza();
+
+    public int getIdObjeto() {
+        return idObjeto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setIdObjeto(int idObjeto) {
+        this.idObjeto = idObjeto;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
     
 }
